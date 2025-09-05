0f2bd913b0491ca1bb2aea1b78c2c873a54f88c2
Hrbzxc.Dev <hrbzxc.dev@gmail.com>
2025-09-06 03:22:11 +0800

    Update URL path for CashSales Details to lowercase for consistency across navigation items.

--- constants/data.ts
+++ constants/data.ts
@@ -11,7 +11,7 @@ export const navItems: NavItem[] = [
   },
   {
     title: 'CashSales Details',
-    url: '/Cashsales-Details',
+    url: '/cashsales-details',
     icon: 'listCollapse',
     isActive: false,
     items: [] // Empty array as there are no child items for CashSales Details
